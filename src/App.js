123ded87097e9632bbed19f8d3a5ac2697faa178
charles <charles.schrumpf@kiswe.com>
2017-10-01 03:31:11 -0400

    youtube embed and title changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import YouTube from 'react-youtube';
 import jah from './jahcorey.jpg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={jah} className="App-logo" alt="logo" />
+          <img src={jah} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to Werb Werb's World</h1>
         </header>
         <p className="App-intro">
-          Shit is coming soon
+          <YouTube
+            videoId="2g811Eo7K8U"
+            opts={opts}
+            onReady={this._onReady}
+          />
         </p>
       </div>
     );
